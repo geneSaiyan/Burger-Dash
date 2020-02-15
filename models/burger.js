139777849c0e896351d72ce11f4011a6d8f66c51
Gene Edwards <eedwards@su.edu>
2020-02-15 18:13:04 -0500

    Added the models folder and burger.js file

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+const orm = require("../config/orm.js");
+
+var burger = {
+    // Retrieve all burgers 
+    selectAll: function (callback) {
+        orm.selectAll("burgers", function (res) {
+            callback(res);
+        });
+    },
+    // Insert a new burger
+    insertOne: function (cols, vals, callback) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            callback(res);
+        });
+    },
+    // Update status of burger devoured column: true or false
+    updateOne: function(objColVals, condition, callback) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          callback(res);
+        });
+      }
+}
+
+module.exports = burger;
