986668cb842767e2ad4ef58007b49bcdbf51c320
Gene Edwards <eedwards@su.edu>
2020-02-15 13:29:53 -0500

    Added node packages and created server.js file

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
