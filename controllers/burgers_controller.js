8bf18ebe8b79cb6dba263f52f6478af780a3dba1
Gene Edwards <eedwards@su.edu>
2020-02-16 01:08:24 -0500

    Added routes and updated index.handlebars page logic

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,9 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Routes
+// Routes Section
+
+// Retrieve all burgers
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
       var hbsObject = {
@@ -15,6 +17,27 @@ router.get("/", function(req, res) {
       res.render("index", hbsObject);
     });
   });
+
+  // Insert a new burger
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+// Retrieve ID of the inserted burger
+      res.json({ id: result.insertId });
+    });
+  });
+
+  // Update devour status to equal true
+router.put("/api/burgers/:id", function(req, res) {
+
+    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404.
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -8,8 +8,21 @@ var app = express();
 app.use(express.static("public"));
 
 // Parse request body as JSON
+// app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
