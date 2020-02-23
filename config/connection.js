6141c7d8302ad3bb6bc239412fc524fd94c2d29c
Gene Edwards <eedwards@su.edu>
2020-02-22 20:03:03 -0500

    Added Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "testpassword",
-  database: "burgers_db"
-});
-
 // Make connection.
+if(process.env.JAWSDB_URL){
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "testpassword",
+    database: "burgers_db"
+  });
+}
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
